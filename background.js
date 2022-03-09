73d0534ea3c2c9f31d557f60b9163cd4e28863a7
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2022-03-08 23:11:30 -0300

    limpieza de codigo comentado

--- background.js
+++ background.js
@@ -1,21 +1,22 @@
-function reddenPage() {
-  // remove element of class andes-money-amount__currency-symbol
+function dolarizar() {
+  // remove element of class andes-money-amount__currency-symbol (signo pesos)
   var elements = document.getElementsByClassName('andes-money-amount__currency-symbol');
   for (var i = 0; i < elements.length; i++) {
     elements[i].parentNode.removeChild(elements[i]);
   }
-  // fetch value from 'https://api.belo.app/public/rate' and store it in variable
+  // fetch rate from 'https://api.belo.app/public/rate' and use it to calculate usdt price
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.belo.app/public/price', true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
+      // si el api responde bien guardar cotizacion en variable rate.
       if (xhr.status === 200) {
-        console.log("hellos")
         var response = JSON.parse(xhr.responseText);
         var rate = response[3].ask;
         console.log(rate)
-        // find all elements of class andes-money-amount__value
+        // find all elements of class andes-money-amount__fraction
         var elements = document.getElementsByClassName('andes-money-amount__fraction');
+        // Procesar todos los precios y marcarlos en rojo en la pagina
         for (var i = 0; i < elements.length; i++) {
           elements[i].style.color = 'red';
           // get the value of the element
@@ -27,31 +28,11 @@ function reddenPage() {
   xhr.send();
 }
 
-/*
-  var url = 'https://api.belo.app/public/rate';
-  console.log("test2")
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onload = function() {
-    console.log("test1")
-      if (this.status == 200) {
-          var data = JSON.parse(this.response);
-          console.log(data);
-          let rate = data[4].rate;
-          var priceTags = document.getElementsByClassName('andes-money-amount__fraction');
-          for (var i = 0; i < priceTags.length; i++) {
-              priceTags[i].style.color = 'red';
-              priceTags[i].innerHTML = ((priceTags[i].innerHTML.replace(".","")) / rate).toFixed(2) + " USDT";
-      
-          }
-      }}
-  
-    console.log("test")
-  }*/
+
   
   chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage
+      function: dolarizar
     });
   });
