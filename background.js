c75d05d80d59c69dbf3ebae5c17647639451bd20
Mateo Cerquetella <mateocerquetella@hotmail.com>
2022-03-08 23:36:37 -0300

    REFACTOR, added new methods to fetch, retrive rates, get and remove elements

--- background.js
+++ background.js
@@ -1,57 +1,57 @@
-function reddenPage() {
-  // remove element of class andes-money-amount__currency-symbol
-  var elements = document.getElementsByClassName('andes-money-amount__currency-symbol');
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].parentNode.removeChild(elements[i]);
+async function renderPage() {
+  async function retrieveRates() {
+    const options = {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'omit',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer'
+    };
+    return await fetchURL('https://api.belo.app/public/price', options);
   }
-  // fetch value from 'https://api.belo.app/public/rate' and store it in variable
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.belo.app/public/price', true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        console.log("hellos")
-        var response = JSON.parse(xhr.responseText);
-        var rate = response[3].ask;
-        console.log(rate)
-        // find all elements of class andes-money-amount__value
-        var elements = document.getElementsByClassName('andes-money-amount__fraction');
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].style.color = 'red';
-          // get the value of the element
-          elements[i].innerHTML = ((elements[i].innerHTML.replace(".","")) / rate).toFixed(2) + " USDT";
-        }
-      }
+
+  async function fetchURL(url = '', options = {}) {
+    const response = await fetch(url, options);
+    return response.json();
+  }
+
+  function retrieveUSDT(rates = []) {
+    return rates.filter((rate) => rate.pairCode === 'USDT/ARS')[0];
+  }
+
+  function getElement(elementName) {
+    return document.getElementsByClassName(elementName);
+  }
+
+  function removeElement(elementName) {
+    const element = getElement(elementName);
+    for (let i = 0; i < element.length; i++) {
+      element[i].parentNode.removeChild(element[i]);
     }
-  };
-  xhr.send();
+  }
+
+  function changeElement(elements) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.color = 'green';
+      elements[i].innerHTML =
+        (elements[i].innerHTML.replace('.', '') / ask).toFixed(2) + ' USDT';
+    }
+  }
+
+  // Main function
+  const rates = await retrieveRates();
+  const { ask } = retrieveUSDT(rates);
+  removeElement('andes-money-amount__currency-symbol');
+  changeElement(getElement('andes-money-amount__fraction'));
 }
 
-/*
-  var url = 'https://api.belo.app/public/rate';
-  console.log("test2")
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onload = function() {
-    console.log("test1")
-      if (this.status == 200) {
-          var data = JSON.parse(this.response);
-          console.log(data);
-          let rate = data[4].rate;
-          var priceTags = document.getElementsByClassName('andes-money-amount__fraction');
-          for (var i = 0; i < priceTags.length; i++) {
-              priceTags[i].style.color = 'red';
-              priceTags[i].innerHTML = ((priceTags[i].innerHTML.replace(".","")) / rate).toFixed(2) + " USDT";
-      
-          }
-      }}
-  
-    console.log("test")
-  }*/
-  
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: reddenPage
-    });
+// Init Chrome extension
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: renderPage
   });
+});
