9942577ae35020d0ed9885118da59c752f414aed
Mateo Cerquetella <mateocerquetella@hotmail.com>
2022-03-21 18:16:12 -0300

    REFACTOR, changed if statement to not for better readability

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -8,25 +8,19 @@ chrome.runtime.onMessage.addListener(async function (
   sender,
   sendResponse
 ) {
-  if (msg.rate) {
-    const rateCode: RatesPair = msg.rate;
-
-    const priceInARS = await meli.getRatesAndCurrency(rateCode);
-
-    const pricingElements = meli.getPricingElements();
-
-    const codeRate = meli.splitRateCode(rateCode);
-
-    const decimals = meli.getCurrencyDecimal(rateCode);
-
-    // Main Price
-    meli.changePricePage(
-      pricingElements,
-      priceInARS,
-      codeRate,
-      decimals
-    );
-  } else {
+  if (!msg.rate) {
     sendResponse('didn`t select a rate or error');
   }
+  const rateCode: RatesPair = msg.rate;
+
+  const priceInARS = await meli.getRatesAndCurrency(rateCode);
+
+  const pricingElements = meli.getPricingElements();
+
+  const codeRate = meli.splitRateCode(rateCode);
+
+  const decimals = meli.getCurrencyDecimal(rateCode);
+
+  // Main Price
+  meli.changePricePage(pricingElements, priceInARS, codeRate, decimals);
 });
