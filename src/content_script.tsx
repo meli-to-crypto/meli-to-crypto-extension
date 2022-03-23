7f4cd82c860835f191a26ce2d8073ef4e3595f0b
Mateo Cerquetella <mateocerquetella@hotmail.com>
2022-03-23 13:57:37 -0300

    REFACTOR, content script

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -5,16 +5,16 @@ const meli = new Meli();
 
 async function retrieveRatesAndChangePage(rateCode: RatesPair) {
   const priceInARS = await meli.getRatesAndCurrency(rateCode);
-
   const pricingElements = meli.getPricingElements();
-
   const codeRate = meli.splitRateCode(rateCode);
-
   const decimals = meli.getCurrencyDecimal(rateCode);
-
-  // Main Price
   meli.changePricePage(pricingElements, priceInARS, codeRate, decimals);
 }
+
+(async function firstLoadOnPage() {
+  const rateCode: RatesPair = RatesPair.USDT_ARS;
+  await retrieveRatesAndChangePage(rateCode);
+})();
 
 // Listen on rates change
 chrome.runtime.onMessage.addListener(async function (
@@ -27,8 +27,3 @@ chrome.runtime.onMessage.addListener(async function (
   }
   await retrieveRatesAndChangePage(msg.rate);
 });
-
-(async function firstLoadOnPage() {
-  const rateCode: RatesPair = RatesPair.USDT_ARS;
-  await retrieveRatesAndChangePage(rateCode);
-})();
