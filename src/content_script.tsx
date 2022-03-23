ee5f9926c178d24ee6bef64c2d07e32552e76b6f
Mateo Cerquetella <mateocerquetella@hotmail.com>
2022-03-23 13:56:05 -0300

    IMPROVE, added first load con page convert to rates

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -3,16 +3,7 @@ import { Meli } from './services/meli';
 
 const meli = new Meli();
 
-chrome.runtime.onMessage.addListener(async function (
-  msg,
-  sender,
-  sendResponse
-) {
-  if (!msg.rate) {
-    sendResponse('didn`t select a rate or error');
-  }
-  const rateCode: RatesPair = msg.rate;
-
+async function retrieveRatesAndChangePage(rateCode: RatesPair) {
   const priceInARS = await meli.getRatesAndCurrency(rateCode);
 
   const pricingElements = meli.getPricingElements();
@@ -23,4 +14,21 @@ chrome.runtime.onMessage.addListener(async function (
 
   // Main Price
   meli.changePricePage(pricingElements, priceInARS, codeRate, decimals);
+}
+
+// Listen on rates change
+chrome.runtime.onMessage.addListener(async function (
+  msg,
+  sender,
+  sendResponse
+) {
+  if (!msg.rate) {
+    sendResponse('didn`t select a rate or error');
+  }
+  await retrieveRatesAndChangePage(msg.rate);
 });
+
+(async function firstLoadOnPage() {
+  const rateCode: RatesPair = RatesPair.USDT_ARS;
+  await retrieveRatesAndChangePage(rateCode);
+})();
